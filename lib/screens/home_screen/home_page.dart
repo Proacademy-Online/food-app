c753b0770047172133a7f1a09096cb97f1bd8e2f
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2021-10-07 14:21:14 +0530

    home screen

--- lib/screens/home_screen/home_page.dart
+++ lib/screens/home_screen/home_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/components/custom_header.dart';
+import 'package:food_app/components/custom_textfeild.dart';
+import 'package:food_app/utils/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SizedBox(
-        child: Center(
-          child: Text("Home"),
-        ),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          CustomHeader(
+            size: size,
+            image: "top.png",
+            widget: Padding(
+              padding: EdgeInsets.only(left: 30, right: 30, top: 66),
+              child: CustomTextfeild(
+                preffix: Icon(
+                  Icons.search,
+                  color: greyColor,
+                ),
+                hintText: 'Find your taste',
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
